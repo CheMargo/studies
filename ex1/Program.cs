44e40aecbb67803b6c14b2acb3d5b904256eff7f
Margarita Cherepanova <ritosnsk@gmail.com>
2022-12-19 12:02:08 +0700

    задачи с семинара

---
+++ ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число");
+double a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double b = Convert.ToInt32(Console.ReadLine());
+double squ = Math.Pow (b, 2);
+
+if (a == squ)
+{
+    Console.WriteLine ("является квадратом");
+}
+else
+{
+        Console.WriteLine ("не является квадратом");
+}
+
